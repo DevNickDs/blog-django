7338bd68282150b0edf4326f52b13e25c21b378f
Nicolas <nikolasdurango@gmail.com>
2023-06-28 15:41:51 -0500

    created views for posts and their categories, then show in templates

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render
+from .models import Post, Category
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(state=True)
+    #print(post)
+    return render(request, 'index.html', {'posts': posts})
 
 def general(request):
-    return render(request, 'general.html')
+    posts = Post.objects.filter(
+        state = True,
+        category = Category.objects.get(name='General')
+    )
+    return render(request, 'general.html', {'posts': posts})
 
 def anime_manga(request):
-    return render(request, 'anime_manga.html')
+    posts = Post.objects.filter(
+        state = True,
+        category = Category.objects.get(name='Anime y Manga')
+    )
+    return render(request, 'anime_manga.html', {'posts': posts})
 
 def technology(request):
-    return render(request, 'technology.html')
+    posts = Post.objects.filter(
+        state = True,
+        category = Category.objects.get(name='Technology')
+    )
+    return render(request, 'technology.html', {'posts': posts})
 
 def video_games(request):
-    return render(request, 'video_games.html')
+    posts = Post.objects.filter(
+        state = True,
+        category = Category.objects.get(name='Gaming')
+    )
+    return render(request, 'video_games.html', {'posts': posts})
