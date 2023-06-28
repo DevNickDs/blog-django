4af3674a1a11d7835e97a6ce242f870b5d08d12b
Nicolas <nikolasdurango@gmail.com>
2023-06-28 01:17:19 -0500

    created views, urls and templates for each blog category

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('general/', views.general, name='general'),
+    path('anime_manga/', views.anime_manga, name='anime_manga'),
+    path('technology/', views.technology, name='technology'),
+    path('video_games/', views.video_games, name='video_games'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def general(request):
+    return render(request, 'general.html')
+
+def anime_manga(request):
+    return render(request, 'anime_manga.html')
+
+def technology(request):
+    return render(request, 'technology.html')
+
+def video_games(request):
+    return render(request, 'video_games.html')
