0ff982c09eb83912651ac66707b66f86f842e663
Nicolas <nikolasdurango@gmail.com>
2023-06-27 19:24:02 -0500

    django-import-export added to project

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, Author
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+class AuthorResource(resources.ModelResource):
+    class Meta:
+        model = Author
+
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name', 'state', 'created_at', 'updated_at')
+    resource_classes = [CategoryResource]
 
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name', 'last_name', 'email']
     list_display = ('name', 'last_name', 'email', 'state', 'created_at', 'updated_at')
+    resource_classes = [AuthorResource]
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Author, AuthorAdmin)
