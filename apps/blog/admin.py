8a96885edd5a0cfccc8c65e0457bbdc74d0de5f1
Nicolas <nikolasdurango@gmail.com>
2023-06-27 19:00:56 -0500

    created models for author and category, added in admin.py

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ('name', 'state', 'created_at', 'updated_at')
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'last_name', 'email']
+    list_display = ('name', 'last_name', 'email', 'state', 'created_at', 'updated_at')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Category name', max_length=100, null=None, blank=None)
+    state = models.BooleanField('Category Activated/Category Deactivated', default=True)
+    created_at = models.DateTimeField('Creation Date', auto_now_add=True)
+    updated_at = models.DateTimeField('Update Date', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField('Author name', max_length=150, null=None, blank=None)
+    last_name = models.CharField('Author last name', max_length=150, null=None, blank=None)
+    email = models.EmailField('Email address', null=None, blank=None)
+    facebook = models.URLField('Facebook' , null=True, blank=True)
+    twitter = models.URLField('Twitter' , null=True, blank=True)
+    instagram = models.URLField('Instagram' , null=True, blank=True)
+    state = models.BooleanField('Active Author/Non-active Author', default=True)
+    created_at = models.DateTimeField('Creation Date', auto_now_add=True)
+    updated_at = models.DateTimeField('Update Date', auto_now=True)
+
+    class Meta:
+        verbose_name = ('Author')
+        verbose_name_plural = ('Authors')
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
